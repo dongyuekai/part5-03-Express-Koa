91a7f93ecd37dfeb49f873f25e0c0942e0b3f24a
“dongyuekai” <457545648@qq.com>
2021-05-27 13:59:28 +0800

    更新任务、删除任务

--- Express/express-demos/01/app.js
+++ Express/express-demos/01/app.js
@@ -4,7 +4,7 @@ const { getDb, saveDb } = require('./db.js')
 
 const app = express()
 
-// 配置解析表单body
+// 中间件 配置解析表单body
 app.use(express.json()) // 解析 application/json 格式数据
 app.use(express.urlencoded()) // 解析application/x-www-form-urlencoded  格式数据
 
@@ -58,6 +58,54 @@ app.post('/todos', async (req, res) => {
     await saveDb(db)
     // 4 发送响应 结束本次请求
     res.status(200).json(todo)
+  } catch (error) {
+    res.status(500).json({
+      error: error.message
+    })
+  }
+})
+
+// 修改
+app.patch('/todos/:id', async (req, res) => {
+  try {
+    // 1 获取表单数据
+    const todo = req.body
+    // 2 查找要修改的任务项
+    const db = await getDb()
+    const ret = db.todos.find(todo => todo.id === Number.parseInt(req.params.id))
+    if (!ret) {
+      // 没找到
+      return res.status(404).end()
+    }
+    // 合并todo到ret 达到修改ret的目的
+    Object.assign(ret, todo)
+    await saveDb(db)
+    res.status(200).json(ret)
+  } catch (error) {
+    res.status(500).json({
+      error: error.message
+    })
+  }
+})
+
+// 删除
+app.delete('/todos/:id', async (req, res) => {
+  try {
+    // 1 获取表单数据
+    const todo = req.body
+    // 2 查找要删除的任务项index
+    const db = await getDb()
+    const retIndex = db.todos.findIndex(todo => todo.id === Number.parseInt(req.params.id))
+    if (retIndex === -1) {
+      // 没找到
+      return res.status(404).end()
+    }
+    db.todos.splice(retIndex, 1)
+    await saveDb(db)
+    res.status(200).json({
+      msg: '删除成功'
+    })
+
   } catch (error) {
     res.status(500).json({
       error: error.message
