72207b13eea37ebb6b7cc2bb368060634ef30143
“dongyuekai” <457545648@qq.com>
2021-05-31 16:38:27 +0800

    koa的中间件合并处理

--- Koa/01/app3.js
+++ Koa/01/app3.js
@@ -1,5 +1,16 @@
 const Koa = require('koa')
+const fs = require('fs')
+const util = require('util')
+const compose = require('koa-compose')
+
 const app = new Koa()
+
+// 异步中间件
+// app.use(async (ctx, next) => {
+//   const data = await util.promisify(fs.readFile)('./views/index.html', 'utf8')
+//   ctx.body = data
+//   next()
+// })
 
 const one = (ctx, next) => {
   console.log('>> one')
@@ -15,9 +26,12 @@ const three = (ctx, next) => {
   console.log('>> three')
   console.log('<< three')
 }
-app.use(one)
-app.use(two)
-app.use(three)
+
+// app.use(one)
+// app.use(two)
+// app.use(three)
+
+app.use(compose([one, two, three]))
 
 app.listen(3000, () => {
   console.log('http://localhost:3000')
