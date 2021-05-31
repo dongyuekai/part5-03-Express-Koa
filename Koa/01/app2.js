60d3494b15e2f1622700fa976f6c58892d0304fd
“dongyuekai” <457545648@qq.com>
2021-05-31 15:54:59 +0800

    koa中的静态资源托管

--- Koa/01/app2.js
+++ Koa/01/app2.js
@@ -1,7 +1,16 @@
 const Koa = require('koa')
 const Router = require('@koa/router')
+const static = require('koa-static')
+const mount = require('koa-mount')
+const path = require('path')
 
 const app = new Koa()
+
+// 静态资源托管
+// app.use(static(path.join(__dirname, './public')))
+// 带虚拟路径的静态资源托管
+app.use(mount('/public', static(path.join(__dirname, './public'))))
+
 const router = new Router()
 
 router.get('/', ctx => {
